8c5f5a8607af90f8ee8f1a0a657aef9caa446bb5
kaushikvenna <vennakaushikramkoteshwar@gmail.com>
2023-10-30 18:07:05 -0500

    (edited icecream.pug and added link for icecream in index.pug)

--- routes/icecream.js
+++ routes/icecream.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('icecream', { title: 'Search Results Icecream' });
+  res.render('icecream', { title: 'Search Results - Icecream' });
 });
 
 module.exports = router;
